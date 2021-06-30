8de4449fe192db23446c34c668fc3dd8da1f4078
davidwolfpaw <david@davidwolfpaw.com>
2021-06-30 16:07:21 -0400

    updates styles, adds footer, updates stylelint

--- stylelint.config.js
+++ stylelint.config.js
@@ -8,8 +8,6 @@ module.exports = {
 		'no-descending-specificity': null,
 		'max-line-length': 255,
 		'selector-class-pattern': '^[a-z0-9_]+(-[a-z0-9_]+)*$',
-		//'font-family-name-quotes': 'always-unless-keyword',
-		//'function-url-quotes': 'always',
 		'declaration-block-no-duplicate-properties': [
 			true,
 			{
